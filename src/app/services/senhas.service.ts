baeaee9b72394873496b40e25583bfa49e375373
Matheus Vitor Fernandes Moreira <matheushifi@gmail.com>
2023-10-09 01:43:26 -0300

    correções de erros

--- src/app/services/senhas.service.ts
+++ src/app/services/senhas.service.ts
@@ -9,6 +9,11 @@ export class SenhasService {
   public senhasExame: number = 0;
   public senhasTotal: number = 0;
   public inputNovaSenha: string = '';
+  public senhasArray: { [key: string]: string[] } = {
+    SG: [],
+    SP: [],
+    SE: [],
+  };
 
   somaGeral() {
     this.senhasGeral++;
@@ -35,7 +40,7 @@ export class SenhasService {
         '-' +
         tipoSenha +
         (this.senhasArray['SG'].length + 1).toString().padStart(2, '0');
-      this.senhasArray.SG.push(this.inputNovaSenha);
+      this.senhasArray['SG'].push(this.inputNovaSenha);
     } else if (tipoSenha == 'SP') {
       this.somaPrior();
       this.inputNovaSenha =
@@ -45,7 +50,7 @@ export class SenhasService {
         '-' +
         tipoSenha +
         (this.senhasArray['SP'].length + 1).toString().padStart(2, '0');
-      this.senhasArray.SP.push(this.inputNovaSenha);
+      this.senhasArray['SP'].push(this.inputNovaSenha);
     } else if ((tipoSenha = 'SE')) {
       this.somaExame();
       this.inputNovaSenha =
@@ -54,8 +59,8 @@ export class SenhasService {
         new Date().getDay().toString().padStart(2, '0') +
         '-' +
         tipoSenha +
-        (this.senhasArray['SE'].length + 1).toString().padstart(2, '0');
-      this.senhasArray.SE.push(this.inputNovaSenha);
+        (this.senhasArray['SE'].length + 1).toString().padStart(2, '0');
+      this.senhasArray['SE'].push(this.inputNovaSenha);
     }
     console.log(this.senhasArray);
   }
